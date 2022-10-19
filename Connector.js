c46cb9e8581f8733d18d8d779bf04130630629fe
NayefFahd <114342287+NayefFahd@users.noreply.github.com>
2022-10-19 16:01:49 +0800

    Assignment 2 files

---
+++ Connector.js
@@ -0,0 +1,37 @@
+var vg_1 = "Plot_Radial_Risk_Factors.json";
+var vg_2 = "Plot_Map_Death_Percentage.json";
+var vg_3 = "Plot_Line_Yearly_Deaths.json";
+var vg_4 = "Plot_Map_Gender.json";
+var vg_5 = "Plot_Bar_Age_Chart.json";
+var vg_6 = "Plot_Bubble_Chart.json";
+var vg_7 = "Plot_Map_Affordablity.json";
+
+
+
+vegaEmbed("#Risk_Factor", vg_1,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#Death_Percentage", vg_2,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#Yearly_Death_Count", vg_3,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#Gender_Map", vg_4,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#Age_Chart", vg_5,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#Bubble_Plot", vg_6,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#Cigarrete_Affordability", vg_7,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
